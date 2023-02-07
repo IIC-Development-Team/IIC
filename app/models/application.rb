9008c1a545fcd436b46751172697ef2b9a5c3941
Zbigniew Przybyła <zprzybyla@tuta.io>
2023-02-07 22:33:27 +0100

    Change database handling. Disable account system. Add contributors page.

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,13 @@
 class Application < ApplicationRecord
+  require 'csv'
+
+  #Delete all data from database to avoid
+  #Problems with IDs
+  Application.delete_all
+
+  #Fill database from CSV file
+  CSV.foreach('apps.csv', headers: true) do |row|
+    Application.create(row.to_h)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root  'home#index'
   get   'home/about'
   get   'home/donate'
+  get   'home/contributors'
 end
