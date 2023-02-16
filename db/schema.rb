bd466788899f56f3cf6675dae48a0b076031a4bf
Zbigniew Przybyła <zprzybyla@tuta.io>
2023-02-16 12:35:27 +0100

    Add app_type(int) and size(float) columns to applications database. Fix some typos.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_140317) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_113105) do
   create_table "applications", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_140317) do
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "size"
+    t.integer "app_type"
   end
 
   create_table "users", force: :cascade do |t|
