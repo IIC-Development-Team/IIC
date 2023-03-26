465237df9af983f5da9eee2e89d036cbf07581f3
Zbigniew Przybyła <zprzybyla@tuta.io>
2023-03-26 23:45:21 +0200

    Delete CSV loading

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,12 +1,3 @@
 class Application < ApplicationRecord
-  require 'csv'
-
-  #Delete all data from database to avoid
-  #Problems with IDs
-  Application.delete_all
-
-  #Fill database from CSV file
-  CSV.foreach('apps.csv', headers: true) do |row|
-    Application.create(row.to_h)
-  end
+ 
 end
