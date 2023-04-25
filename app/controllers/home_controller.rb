45429624c7a42dcae2fbbad076d45ed026bd7e74
Zbigniew Przybyla <zprzybyla@tuta.io>
2023-04-25 13:36:05 +0200

    Add exporting data to csv

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class HomeController < ApplicationController
   def index
     ua = request.user_agent
@@ -9,6 +11,19 @@ class HomeController < ApplicationController
     @applications = Application.all
 
     @usrs = Usr.all
+  end
+
+  def export
+    @data = Application.all
+    headers = ['id', 'title', 'author', 'rd', 'ca', 'cs', 'img', 'created_at', 'updated_at', 'size', 'app_type']
+    csv_data = CSV.generate(headers: true) do |csv|
+      csv << headers
+      @data.each do |row|
+        csv << [row.id, row.title, row.author, row.rd, row.ca, row.cs, row.img, row.created_at, row.updated_at, row.size, row.app_type]
+      end
+    end
+
+    send_data csv_data, filename: "apps.csv"
   end
 
   def clients
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get   'wallet/control', to: "wallets#control"
   get   'home/control'
   get   'home/clients'
+  get   'export', to: "home#export", as:'export'
 end
